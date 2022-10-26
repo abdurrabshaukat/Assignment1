82e9eb4384999821573a41beded2c8e4f3ebe03f
abdurrabshaukat <abdurrabshaukat@gmail.com>
2022-10-26 18:12:47 +0300

    Question 5

---
+++ Question5.java
@@ -0,0 +1,32 @@
+public class Question5{
+	
+	public  static void main(String args[]){
+		int n = 14;
+		
+		for(int i = 0 ; i < n ; i++){
+			for(int j = 0; j < n; j++){
+				if(				
+					(i == 0 || i == n-1 )
+					||
+					( i <= 6 && j <= (n/2)-i )
+					||
+					( i >= 7  && j < i-4 )
+					
+				)System.out.print("*");
+				else
+					System.out.print(" ");				
+			}
+			System.out.println();
+		
+		
+		
+		
+		
+		}
+		
+		
+	}
+	
+	
+	
+}
